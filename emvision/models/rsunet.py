144bfb6ed6d13e566534cb973cde094925e5db4f
torms3 <kisuklee@mit.edu>
2018-03-10 10:56:36 -0500

    make it shorter

--- emvision/models/rsunet.py
+++ emvision/models/rsunet.py
@@ -13,33 +13,6 @@ def conv(in_channels, out_channels, kernel_size=3, stride=1, bias=False):
                      stride=stride, padding=padding, bias=bias)
 
 
-class ResBlock(nn.Module):
-    def __init__(self, channels):
-        super(ResBlock, self).__init__()
-
-        self.norm1 = nn.BatchNorm3d(channels)
-        self.relu1 = nn.ReLU(inplace=True)
-        self.conv1 = conv(channels, channels)
-
-        self.norm2 = nn.BatchNorm3d(channels)
-        self.relu2 = nn.ReLU(inplace=True)
-        self.conv2 = conv(channels, channels)
-
-    def forward(self, x):
-        residual = x
-
-        out = self.norm1(x)
-        out = self.relu1(out)
-        out = self.conv1(out)
-
-        out = self.norm2(out)
-        out = self.relu2(out)
-        out = self.conv2(out)
-
-        out += residual
-        return out
-
-
 class BNReLUConv(nn.Sequential):
     def __init__(self, in_channels, out_channels, kernel_size=3):
         super(BNReLUConv, self).__init__()
@@ -49,11 +22,25 @@ class BNReLUConv(nn.Sequential):
                                      kernel_size=kernel_size))
 
 
+class ResBlock(nn.Module):
+    def __init__(self, channels):
+        super(ResBlock, self).__init__()
+        self.conv1 = BNReLUConv(channels, channels)
+        self.conv2 = BNReLUConv(channels, channels)
+
+    def forward(self, x):
+        residual = x
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x += residual
+        return x
+
+
 class ConvBlock(nn.Sequential):
     def __init__(self, in_channels, out_channels):
         super(ConvBlock, self).__init__()
-        self.add_module('pre', BNReLUConv(in_channels, out_channels))
-        self.add_module('res', ResBlock(out_channels))
+        self.add_module('pre',  BNReLUConv(in_channels, out_channels))
+        self.add_module('res',  ResBlock(out_channels))
         self.add_module('post', BNReLUConv(out_channels, out_channels))
 
 
@@ -78,35 +65,27 @@ class RSUNet(nn.Module):
         assert len(width) > 1
         depth = len(width) - 1
 
-        # InputBlock.
         self.iconv = ConvBlock(width[0], width[0])
 
-        # Contracting/downsampling pathway.
         self.dconvs = nn.ModuleList()
         for d in range(depth):
             self.dconvs.append(nn.Sequential(nn.MaxPool3d((1,2,2)),
                                              ConvBlock(width[d], width[d+1])))
-
-        # Expanding/upsampling pathway.
         self.uconvs = nn.ModuleList()
         for d in reversed(range(depth)):
             self.uconvs.append(nn.Sequential(UpBlock(width[d+1], width[d]),
                                              ConvBlock(width[d], width[d])))
 
-        # Initialize weights.
         self.init_weights()
 
     def forward(self, x):
-        # InputBlock.
         x = self.iconv(x)
 
-        # Contracting/downsampling pathway.
         skip = list()
         for dconv in self.dconvs:
             skip.append(x)
             x = dconv(x)
 
-        # Expanding/upsampling pathway.
         for uconv in self.uconvs:
             x = uconv(x, skip.pop())
 
